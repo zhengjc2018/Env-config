4dcf050a7503fc000fd3a2f68f9e969a09787014
zhengjc2018 <zhengjc1995@gamil>
2020-04-14 23:01:09 +0800

    update: docker-compose添加国内镜像源

--- docker/docker安装.md
+++ docker/docker安装.md
@@ -20,6 +20,11 @@
 ```
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 ```
+1.1.1. 使用国内镜像下载
+```
+sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+```
+
 1.2. Apply executable permissions to the binary
 ```
 sudo chmod +x /usr/local/bin/docker-compose
--- docker/ubuntu_docker_run.sh
+++ docker/ubuntu_docker_run.sh
@@ -1,4 +1,6 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
@@ -16,5 +18,5 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 sudo docker run hello-world
